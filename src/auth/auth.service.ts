8eb339cac749ad3e216fbb4ffe9e535d9e71f4bb
nicolas <digitalcore.ar@gmail.com>
2025-08-12 10:29:34 -0300

    modulo auth en proceso

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { LoginDto } from './dto/login-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) { }
 
   getJwtToken(payload: JwtPayload) {
     return this.jwtService.sign(payload);
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.userRepository.findOneBy({ email });
+
+    if (!user) {
+      throw new UnauthorizedException('Credentials are not valid!');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Credentials are not valid!');
+    }
+
+    return {
+      token: this.getJwtToken({ id: user.id }),
+    }
+
   }
 }
--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -9,8 +9,5 @@ export class LoginDto {
     @IsNotEmpty()
     @MinLength(6)
     @MaxLength(50)
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/, {
-        message: 'Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-    })
     password: string;
 }
